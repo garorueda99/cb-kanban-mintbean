f1f021b2c2c6f5045ddddeae9f8e1c14d5d0250b
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-08-28 16:49:39 -0400

    added GlobalStyles CSS reset + google font lato

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+// ### IMPORT COMPONENTS ###
+
+import GlobalStyles from "./components/GlobalStyles";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <h1>
+      Hello World
+      <GlobalStyles />
+    </h1>
+  );
 }
 
 export default App;
