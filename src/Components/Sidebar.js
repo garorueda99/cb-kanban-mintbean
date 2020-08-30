7f5ba16866afdba1d13fbd5d4addc595a796e95c
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-08-29 21:45:10 -0400

    updated sidebar component

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,66 +1,160 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import uuid from "uuid/v4";
 import CreateColumn from "./CreateColumn";
+import { addColumn } from "../actions";
+import COLORS from "./COLORS";
+import { FiArrowLeft, FiEdit, FiDelete } from "react-icons/fi";
 
-import { FiHome } from "react-icons/fi";
+// ## TIPPY TOOLTIPS ##
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/themes/material.css";
+import "tippy.js/animations/scale-subtle.css";
 
 const Sidebar = () => {
-  return (
-    <StyledAside className="sidebar-nav">
-      <StyledDiv>
-        <StyledUl>
-          <StyledLi>
-            <StyledNavLink exact to="/">
-              <FiHome style={{ marginRight: "10px" }} />
-              Home
-            </StyledNavLink>
-          </StyledLi>
-        </StyledUl>
+  const id = uuid();
+  let name = "New column";
+  const dispatch = useDispatch();
 
-        <CreateColumn />
-      </StyledDiv>
-    </StyledAside>
+  const column = {
+    name,
+    items: [],
+  };
+
+  return (
+    <Wrapper>
+      <Container>
+        <SidebarList>
+          <li>
+            <Tippy
+              content={"Return back to landing page"}
+              placement="right"
+              animation="scale-subtle"
+              theme="material"
+              arrow={true}
+              duration={200}
+              delay={[75, 0]}
+              distance={8}
+            >
+              <StyledNavLink exact to="/">
+                <FiArrowLeft size={36} />
+              </StyledNavLink>
+            </Tippy>
+          </li>
+          <li>
+            <Tippy
+              content={"Add a new Column"}
+              placement="right"
+              animation="scale-subtle"
+              theme="material"
+              arrow={true}
+              duration={200}
+              delay={[75, 0]}
+              distance={8}
+            >
+              <Button onClick={() => dispatch(addColumn(column, id))}>
+                <FiEdit size={36} />
+              </Button>
+            </Tippy>
+          </li>
+          <li>
+            <Tippy
+              content={"Clear all empty Columns"}
+              placement="right"
+              animation="scale-subtle"
+              theme="material"
+              arrow={true}
+              duration={200}
+              delay={[75, 0]}
+              distance={8}
+            >
+              <Button onClick={() => console.log("Clear All Tasks")}>
+                <FiDelete size={36} />
+              </Button>
+            </Tippy>
+          </li>
+        </SidebarList>
+      </Container>
+    </Wrapper>
   );
 };
 
-const StyledAside = styled.aside`
-  min-width: 140px;
-  border-right: 1px solid gainsboro;
-  margin-right: 40px;
-`;
-
-const StyledDiv = styled.div`
+const Wrapper = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
-  align-items: center;
+  margin-right: 2%;
 `;
 
-const StyledNavLink = styled(NavLink)`
-  font-weight: bold;
-  text-decoration: none;
-  color: #000;
-  padding: 10px;
+const Container = styled.div`
+  min-width: 100px;
+  min-height: 100vh;
+  padding: 5%;
+  /* border: 1px solid red; */
+
+  box-shadow: 0px 2px 2px 2px rgba(211, 211, 211, 0.75);
+  border-right: 1px solid gainsboro;
+`;
+
+const SidebarList = styled.ul`
+  padding: 5%;
+  /* border: 3px dashed red; */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  & li {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    margin: 15% 0;
+  }
+`;
+
+const Button = styled.button`
+  background: transparent;
+  border: none;
+  outline: none;
+  cursor: pointer;
 
   &:hover {
-    background-color: #cab8f8;
-    color: blue;
+    color: ${COLORS.btnSecondary};
+  }
+
+  &:focus {
+    color: ${COLORS.btnSecondary};
+  }
+
+  &:active {
+    transform: scale(1.3);
+    color: ${COLORS.btnSecondary};
+  }
+`;
+
+const StyledNavLink = styled(Link)`
+  /* color: ${COLORS.btnSecondary}; */
+
+  &:hover {
+    color: ${COLORS.btnSecondary};
     border-radius: 30px;
     cursor: pointer;
   }
 
-  &.active {
-    color: blue;
+  &:focus {
+    outline: none;
+    /* transform: scale(1.5); */
+    color: ${COLORS.btnSecondary};
   }
-`;
 
-const StyledUl = styled.ul`
-  padding: 0;
-`;
-
-const StyledLi = styled.li`
-  margin: 5px 0 5px 5px;
-  padding: 8px;
+  &:active {
+    transform: scale(1.3);
+    color: ${COLORS.btnSecondary};
+  }
 `;
 
 export default Sidebar;
