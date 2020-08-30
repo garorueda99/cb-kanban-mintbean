250e60ff07a2da8956d9d8c03863f99672957da0
donnyesq <adonis@darmas.ca>
2020-08-30 14:17:47 -0400

    persisting data to localStorage complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 // ### IMPORT COMPONENTS ###
 import GlobalStyles from "./components/GlobalStyles";
@@ -8,6 +9,12 @@ import LandingPage from "./components/LandingPage";
 import WarningModal from "./components/WarningModal";
 
 function App() {
+  const state = useSelector((state) => state);
+
+  React.useEffect(() => {
+    window.localStorage.setItem("persistedState", JSON.stringify(state));
+  }, [state]);
+
   return (
     <>
       <WarningModal />
