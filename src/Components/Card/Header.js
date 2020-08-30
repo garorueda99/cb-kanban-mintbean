d514a27740b6407bdb37a1b04f95a4d1558dc74d
donnyesq <adonis@darmas.ca>
2020-08-30 18:19:47 -0400

    card styling complete

--- src/Components/Card/Header.js
+++ src/Components/Card/Header.js
@@ -1,41 +1,43 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import { deepOrange } from '@material-ui/core/colors';
-import moment from 'moment';
-import { makeStyles } from '@material-ui/core/styles';
-import styled from 'styled-components';
-import TextField from '@material-ui/core/TextField';
+import React from "react";
+// import Avatar from "@material-ui/core/Avatar";
+import { deepOrange } from "@material-ui/core/colors";
+import moment from "moment";
+import { makeStyles } from "@material-ui/core/styles";
+import styled from "styled-components";
+import TextField from "@material-ui/core/TextField";
+import { FiUser } from "react-icons/fi";
 
-const date = moment(new Date()).format('MMM Do');
+const date = moment(new Date()).format("MMM Do");
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    '& > *': {
-      margin: theme.spacing(1),
-    },
-  },
-  orange: {
-    color: theme.palette.getContrastText(deepOrange[500]),
-    backgroundColor: deepOrange[500],
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: "flex",
+//     "& > *": {
+//       margin: theme.spacing(1),
+//     },
+//   },
+//   orange: {
+//     color: theme.palette.getContrastText(deepOrange[500]),
+//     backgroundColor: deepOrange[500],
+//   },
+// }));
 
-export default function Header({ title = 'Set a Title', setTitle }) {
-  const classes = useStyles();
+export default function Header({ title = "Set a Title", setTitle }) {
+  // const classes = useStyles();
 
   return (
     <div>
       <Wrapper>
-        <Avatar
+        {/* <Avatar
           alt='user'
           src='/static/images/avatar/avatar1.png'
           className={classes.orange}
-        />
-        <div style={{ marginLeft: '8px' }}>
+        /> */}
+        <Avatar />
+        <div style={{ marginLeft: "8px" }}>
           <TextField
-            id='standard-basic'
-            label='Title'
+            id="standard-basic"
+            label="Title"
             defaultValue={title}
             onChange={(e) => setTitle(e.target.value)}
           />
@@ -52,13 +54,17 @@ const Wrapper = styled.div`
   margin-bottom: 16px;
 `;
 
+const Avatar = styled(FiUser)`
+  color: #fff;
+  background: deepskyblue;
+  border: 5px solid deepskyblue;
+  border-radius: 50%;
+  font-size: 48px;
+`;
+
 const TimeStamp = styled.div`
   margin-top: 6px;
   font-style: italic;
   font-size: 0.7rem;
   color: rgb(101, 119, 134);
-  ::before {
-    content: '•';
-    margin: 0 5px;
-  }
 `;
