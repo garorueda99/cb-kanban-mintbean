e6a930567980dba15eaa9b502286e01dae94a22f
Andres Rueda <garo99@gmail.com>
2020-08-29 20:34:21 -0400

    add card

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,11 +1,12 @@
-import React from "react";
-import styled from "styled-components";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-import Card from "../components/Card";
-import Sidebar from "./Sidebar";
-import { useSelector } from "react-redux";
-import { updateColumnPositionH, updateColumnPositionV } from "../actions";
-import { useDispatch } from "react-redux";
+import React from 'react';
+import styled from 'styled-components';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import Card from '../components/Card';
+import Sidebar from './Sidebar';
+import { useSelector } from 'react-redux';
+import { updateColumnPositionH, updateColumnPositionV } from '../actions';
+import { useDispatch } from 'react-redux';
+import { FiPlusCircle } from 'react-icons/fi';
 
 const Board = () => {
   const dispatch = useDispatch();
@@ -49,7 +50,7 @@ const Board = () => {
   return !columns ? (
     <div>Loading...</div>
   ) : (
-    <div style={{ display: "flex" }}>
+    <div style={{ display: 'flex' }}>
       <Sidebar />
       <StyledDiv>
         <DragDropContext onDragEnd={(result) => onDragEnd(result, columns)}>
@@ -57,16 +58,21 @@ const Board = () => {
             return (
               <div
                 style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  border: "2px solid purple",
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  border: '2px solid purple',
                 }}
                 key={columnId}
               >
                 <input value={column.name} />
 
                 <div style={{ margin: 8 }}>
+                  <CircleIcon
+                    onClick={() => {
+                      console.log('New Card Added');
+                    }}
+                  />
                   <Droppable droppableId={columnId} key={columnId}>
                     {(provided, snapshot) => {
                       return (
@@ -75,12 +81,12 @@ const Board = () => {
                           ref={provided.innerRef}
                           style={{
                             border: snapshot.isDraggingOver
-                              ? "1px solid gainsboro"
-                              : "1px solid white",
+                              ? '1px solid gainsboro'
+                              : '1px solid white',
                             padding: 4,
                             width: 250,
                             minHeight: 500,
-                            border: "2px solid green",
+                            border: '2px solid green',
                           }}
                         >
                           {column.items.map((item, index) => {
@@ -97,15 +103,15 @@ const Board = () => {
                                       {...provided.draggableProps}
                                       {...provided.dragHandleProps}
                                       style={{
-                                        position: "relative",
+                                        position: 'relative',
                                         padding: 16,
-                                        margin: "0 0 8px 0",
+                                        margin: '0 0 8px 0',
                                         boxShadow: snapshot.isDragging
-                                          ? "0px 0px 13px -1px rgba(168,168,168,0.6)"
-                                          : "0px 0px 13px -1px rgba(168,168,168,0.3)",
-                                        color: "#000",
-                                        borderRadius: "10px",
-                                        border: "2px solid pink",
+                                          ? '0px 0px 13px -1px rgba(168,168,168,0.6)'
+                                          : '0px 0px 13px -1px rgba(168,168,168,0.3)',
+                                        color: '#000',
+                                        borderRadius: '10px',
+                                        border: '2px solid pink',
                                         ...provided.draggableProps.style,
                                       }}
                                     >
@@ -136,6 +142,14 @@ const StyledDiv = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
+`;
+
+const CircleIcon = styled(FiPlusCircle)`
+  margin: 5px;
+  &:hover {
+    cursor: pointer;
+    color: #0acc33;
+  }
 `;
 
 export default Board;
