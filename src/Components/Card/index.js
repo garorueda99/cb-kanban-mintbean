be9d32f4f8d2700ad5962f0ecc05137c83184773
Andres Rueda <garo99@gmail.com>
2020-08-30 21:30:41 -0400

    delete on card

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
-import styled, { keyframes } from "styled-components";
-import Header from "./Header";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import { useDispatch } from "react-redux";
-import { saveCardInfo } from "../../actions";
-import Dialog from "@material-ui/core/Dialog";
+import React, { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+import Header from './Header';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { useDispatch } from 'react-redux';
+import { saveCardInfo, deleteCard } from '../../actions';
+import Dialog from '@material-ui/core/Dialog';
 
-import { makeStyles } from "@material-ui/core/styles";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 const close = keyframes`
 0%{
@@ -38,7 +38,7 @@ export default function Card({ columnId, item, cardStatus, setCardStatus }) {
   const dispatch = useDispatch();
 
   const classes = useStyles();
-  const [priority, setPriority] = React.useState("Low");
+  const [priority, setPriority] = React.useState('Low');
 
   const handleChange = (event) => {
     setPriority(event.target.value);
@@ -46,18 +46,18 @@ export default function Card({ columnId, item, cardStatus, setCardStatus }) {
   };
 
   return (
-    <Dialog open={cardStatus} aria-labelledby="form-dialog-title">
+    <Dialog open={cardStatus} aria-labelledby='form-dialog-title'>
       {item !== null ? (
         <CardWrapper>
           <Header title={item.content} setTitle={setTitle} />
           <ContentWrapper>
             <TextField
-              id="outlined-multiline-static"
-              label="Description"
+              id='outlined-multiline-static'
+              label='Description'
               multiline
               rows={4}
               defaultValue={item.task}
-              variant="outlined"
+              variant='outlined'
               onChange={(e) => {
                 setInfo(e.target.value);
               }}
@@ -65,35 +65,45 @@ export default function Card({ columnId, item, cardStatus, setCardStatus }) {
             />
 
             <FormControl className={classes.formControl}>
-              <InputLabel id="demo-simple-select-label">
+              <InputLabel id='demo-simple-select-label'>
                 Priority Level
               </InputLabel>
               <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
+                labelId='demo-simple-select-label'
+                id='demo-simple-select'
                 value={priority}
                 onChange={handleChange}
               >
-                <MenuItem value={"Low"}>Low</MenuItem>
-                <MenuItem value={"Medium"}>Medium</MenuItem>
-                <MenuItem value={"High"}>High</MenuItem>
+                <MenuItem value={'Low'}>Low</MenuItem>
+                <MenuItem value={'Medium'}>Medium</MenuItem>
+                <MenuItem value={'High'}>High</MenuItem>
               </Select>
             </FormControl>
 
             <Button
-              variant="contained"
+              variant='contained'
               onClick={() => {
                 setCardStatus((n) => !n);
                 dispatch(saveCardInfo(columnId, item, info, title));
               }}
-              style={{ color: "white", background: "#296d98" }}
+              style={{ color: 'white', background: '#296d98' }}
             >
               SAVE
+            </Button>
+            <Button
+              variant='contained'
+              onClick={() => {
+                setCardStatus((n) => !n);
+                dispatch(deleteCard(columnId, item.id));
+              }}
+              style={{ color: 'white', background: '#296d98' }}
+            >
+              DELETE
             </Button>
           </ContentWrapper>
         </CardWrapper>
       ) : (
-        "LOADING"
+        'LOADING'
       )}
     </Dialog>
   );
